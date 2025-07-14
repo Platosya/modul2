e228d8ba6be704f37fb1fcbc50a6cb2b9e1bf17b
Platon <kryshtopa2010@gmail.com>
2025-07-14 13:50:28 -0400

    new method added

--- posts.py
+++ posts.py
@@ -7,9 +7,9 @@ cursor.execute('''CREATE TABLE users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password_hash TEXT NOT NULL
- );
+ );''')
 
-CREATE TABLE posts (
+cursor.execute('''CREATE TABLE posts (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title TEXT NOT NULL,
   content TEXT NOT NULL,
