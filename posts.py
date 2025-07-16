61e0f14a54822756ca7ca9bcd8a088b007a6611d
Platon <kryshtopa2010@gmail.com>
2025-07-16 13:54:23 -0400

    added registration

--- posts.py
+++ posts.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request,redirect,url_for
 import sqlite3
+
+from werkzeug.security import generate_password_hash
+
 app = Flask(__name__)
 connection = sqlite3.connect('db.db', check_same_thread=False)
 cursor = connection.cursor()
-cursor.execute('''CREATE TABLE users (
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password_hash TEXT NOT NULL
  );''')
 
-cursor.execute('''CREATE TABLE posts (
+cursor.execute('''CREATE TABLE IF NOT EXISTS posts (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title TEXT NOT NULL,
   content TEXT NOT NULL,
@@ -53,6 +56,23 @@ def post(post_id):
     ).fetchone()
     post_dict={'id':result[0],'title':result[1],'content':result[2]}
     return render_template('post.html',post=post_dict)
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        try:
+            cursor.execute('INSERT INTO user (username password_hash)VALUES(?,?)',
+                           (username, generate_password_hash(password,
+                                                            method='pbkdf2:sha256'))
+                           )
+            connection.commit()
+            print('Ur connection has been made sucesfully')
+        except sqlite3.IntegrityError:
+            return render_template('register.html',
+                                   message= 'Username already exists!')
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run()
